297c333f02d55082311f192fe5b408b36ef04fc4
slont <s.lontzetidis@gmail.com>
2021-12-20 18:23:37 +0200

    Created script to automate the conversion of natural language case to an OWL individual in the cyber crime ontology, along with their respective data property associations

---
+++ main.py
@@ -0,0 +1,186 @@
+from owlready2 import *
+
+crime = get_ontology("file://CyberCrimeOntology.owl").load()
+
+# wordlists for each crime characteristic
+
+# offender characteristics
+
+cyber_criminal_list=['criminal','cyber criminal','cyber-criminal']
+cyber_fighter_list=['fighter']
+cyber_terrorist_list=['terrorism', 'terrorist']
+hacktivist_list=['hacktivist','political','politics']
+insider_list=['insider','malicious employee','employee']
+online_social_hacker_list=['social hacker', 'online social hacker']
+script_kiddie_list=['kiddie', 'script kiddie']
+sexually_deviant_user_list=['sexually','deviant']
+entities_as_offenders_list=['offenders','espionage']
+
+# access violation
+physical_tampering_list=['physical access', 'physical computer access', 'physical tampering']
+local_computer_access_list=['local access','local computer access']
+remote_computer_access_list=['remote access', 'remote computer access']
+
+# target
+
+physical_abuse_list=['physical assault','physical battery','murder','death']
+emotional_abuse_list=['mental anguish','offending sanity','offending privacy']
+sexual_abuse_list=['pornographic','porn','sexual assault','sexual battery','rape','raping','pornography']
+financial_abuse_list=['management of funds','unauthorized use of assets','unauthorized use of resources']
+ict_abuse_list=['ICT abuse']
+infrastructure_ict_abuse_list=['infrastructure ICT abuse']
+social_abuse_list=['afflicting prosperity','afflicting stability','afflicting prosperity','inaccessible services to civilians']
+
+# victim
+
+kid_list=['kid','kids','child','children']
+adult_list=['adult', 'adults']
+company_list=['company','organization','employee']
+country_list=['country','state']
+
+# harm
+
+individual_harm_list=['loss of life','moral harm','physical injury','substantial damage','fear','emotional distress']
+aggregated_individual_harm_list=['moral harm','accumulated loss of property','emotional distress','fear']
+generalized_individual_harm_list=['dispossession of wealth','moral decay','social disorder']
+direct_systemic_harm_list=['direct','chaos','anarchy','country engagement']
+inchoate_harm_list=['inferential','potential','inchoate']
+
+add_more=True
+while(add_more):
+
+    crime_name=input("Insert name for the case (STOP to end process): ")
+
+    if crime_name!="STOP":
+
+        # create individual without assigned class
+
+        new_case=Thing(name=str(crime_name),namespace=crime)
+        crime_case=input("Insert case description: ")
+
+        # checking for offender characteristics in crime case
+
+        for x in cyber_criminal_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Cyber Criminal']
+
+        for x in cyber_fighter_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Cyber Fighter']
+
+        for x in hacktivist_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Hacktivist']
+
+        for x in cyber_terrorist_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Cyber Terrorist']
+
+        for x in script_kiddie_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Script Kiddie']
+
+        for x in insider_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Insider']
+
+        for x in online_social_hacker_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Online Social Hacker']
+
+        for x in entities_as_offenders_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Entities as Offenders']
+
+        for x in sexually_deviant_user_list:
+            if crime_case.__contains__(x):
+                new_case.hasOffender = ['Sexually Deviant User']
+
+        # checking for access violation characteristics
+
+        for x in physical_tampering_list:
+            if crime_case.__contains__(x):
+                new_case.hasAccessViolation = ['Physical Tampering']
+
+        for x in local_computer_access_list:
+            if crime_case.__contains__(x):
+                new_case.hasAccessViolation = ['Local Computer Access']
+
+        for x in remote_computer_access_list:
+            if crime_case.__contains__(x):
+                new_case.hasAccessViolation = ['Remote Computer Access']
+
+        # checking for target characteristics
+
+        for x in physical_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Physical Abuse']
+
+        for x in emotional_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Emotional Abuse']
+
+        for x in social_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Social Abuse']
+
+        for x in sexual_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Sexual Abuse']
+
+        for x in financial_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Financial Abuse']
+
+        for x in ict_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['ICT Abuse']
+
+        for x in infrastructure_ict_abuse_list:
+            if crime_case.__contains__(x):
+                new_case.hasTarget = ['Infrastructure ICT Abuse']
+
+        # checking for victim characteristics
+
+        for x in kid_list:
+            if crime_case.__contains__(x):
+                new_case.hasVictim = ['Kid']
+
+        for x in adult_list:
+            if crime_case.__contains__(x):
+                new_case.hasVictim = ['Adult']
+
+        for x in company_list:
+            if crime_case.__contains__(x):
+                new_case.hasVictim = ['Company']
+
+        for x in country_list:
+            if crime_case.__contains__(x):
+                new_case.hasVictim = ['Country']
+
+        # checking for harm characteristics
+
+        for x in individual_harm_list:
+            if crime_case.__contains__(x):
+                new_case.hasHarm = ['Individual Harm']
+
+        for x in generalized_individual_harm_list:
+            if crime_case.__contains__(x):
+                new_case.hasHarm = ['Generalized Individual Harm']
+
+        for x in aggregated_individual_harm_list:
+            if crime_case.__contains__(x):
+                new_case.hasHarm = ['Aggregated Individual Harm']
+
+        for x in direct_systemic_harm_list:
+            if crime_case.__contains__(x):
+                new_case.hasHarm = ['Direct Systemic Harm']
+
+        for x in inchoate_harm_list:
+            if crime_case.__contains__(x):
+                new_case.hasHarm = ['Inchoate Harm']
+
+    else:
+        add_more=False
+
+crime.save("Cyber_crime_ontology_with_new_crime_cases.owl")
