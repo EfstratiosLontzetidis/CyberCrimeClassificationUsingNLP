91d97e80d769e95a0111b2b934d62b14870519fd
slont <s.lontzetidis@gmail.com>
2021-12-21 15:41:06 +0200

    Added NLP function #1 , to do the same process to the case input. Also updated the check to perform between crime characteristics and individuals using NLP processed lists (case and characteristics).

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from nltk import *
 from nltk import bigrams, trigrams
 
 def nlp_most_words_in_description(filename):
-    # input abusive user description from file
+    # input description from file
     with open(filename, "r") as myfile:
         case = myfile.read()
 
@@ -43,6 +43,37 @@ def nlp_most_words_in_description(filename):
 
     return listtoreturn
 
+def nlp_case(case):
+    # tokenization
+    tokens = nltk.word_tokenize(case)
+    # stopwords and special characters
+    wordlist = nltk.corpus.stopwords.words("english")
+    wordlist2 = [",", ".", "’", ":", "“", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "''", "``", "'", "The"]
+    # stemming if needed
+    pst = PorterStemmer()
+    # remove useless tokens
+    new = []
+    # if needed to switch to bigrams or trigrams
+    bigrams = nltk.bigrams(list(tokens))
+    trigrams = nltk.trigrams(list(tokens))
+    # remove unnecessary words & special chars
+    for words in tokens:
+        # frequency
+        flag = True
+        for x in wordlist:
+            if x == words or words in wordlist2:
+                flag = False
+                break
+        if flag == True:
+            # new.append(words)
+            new.append(pst.stem(words))
+
+
+    return new
+
+def printme(list):
+    print(list)
+
 crime = get_ontology("file://CyberCrimeOntology.owl").load()
 
 # wordlists for each crime characteristic
@@ -50,6 +81,7 @@ crime = get_ontology("file://CyberCrimeOntology.owl").load()
 # offender characteristics
 
 abusive_user_list = nlp_most_words_in_description("./descriptions/characteristics/offender/abusive user.txt")
+printme(abusive_user_list)
 cyber_bully_list = ['bully', 'cyber-bully', 'cyber bully']
 cyber_criminal_list = ['criminal', 'cyber criminal', 'cyber-criminal']
 cyber_fighter_list = ['fighter']
@@ -105,163 +137,164 @@ while (add_more):
 
         new_case = Thing(name=str(crime_name), namespace=crime)
         crime_case = input("Insert case description: ")
+        crime_case_nlp=nlp_case(crime_case)
 
         # checking for offender characteristics in crime case
 
         for x in abusive_user_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Abusive User']
                 break
 
         for x in cyber_bully_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Cyber Bully']
                 break
 
         for x in cyber_criminal_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Cyber Criminal']
                 break
 
         for x in cyber_fighter_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Cyber Fighter']
                 break
 
         for x in hacktivist_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Hacktivist']
 
         for x in cyber_terrorist_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Cyber Terrorist']
                 break
 
         for x in script_kiddie_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Script Kiddie']
                 break
 
         for x in insider_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Insider']
                 break
 
         for x in online_social_hacker_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Online Social Hacker']
                 break
 
         for x in entities_as_offenders_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Entities as Offenders']
                 break
 
         for x in sexually_deviant_user_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasOffender = ['Sexually Deviant User']
                 break
 
         # checking for access violation characteristics
 
         for x in physical_tampering_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasAccessViolation = ['Physical Tampering']
                 break
 
         for x in local_computer_access_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasAccessViolation = ['Local Computer Access']
                 break
 
         for x in remote_computer_access_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasAccessViolation = ['Remote Computer Access']
                 break
 
         # checking for target characteristics
 
         for x in physical_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Physical Abuse']
                 break
 
         for x in emotional_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Emotional Abuse']
                 break
 
         for x in social_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Social Abuse']
                 break
 
         for x in sexual_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Sexual Abuse']
                 break
 
         for x in financial_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Financial Abuse']
                 break
 
         for x in ict_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['ICT Abuse']
                 break
 
         for x in infrastructure_ict_abuse_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasTarget = ['Infrastructure ICT Abuse']
                 break
 
         # checking for victim characteristics
 
         for x in kid_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasVictim = ['Kid']
                 break
 
         for x in adult_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasVictim = ['Adult']
                 break
 
         for x in company_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasVictim = ['Company']
                 break
 
         for x in country_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasVictim = ['Country']
                 break
 
         # checking for harm characteristics
 
         for x in individual_harm_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasHarm = ['Individual Harm']
                 break
 
         for x in generalized_individual_harm_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasHarm = ['Generalized Individual Harm']
                 break
 
         for x in aggregated_individual_harm_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasHarm = ['Aggregated Individual Harm']
                 break
 
         for x in direct_systemic_harm_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasHarm = ['Direct Systemic Harm']
                 break
 
         for x in inchoate_harm_list:
-            if crime_case.__contains__(x):
+            if x in crime_case_nlp:
                 new_case.hasHarm = ['Inchoate Harm']
                 break
 
